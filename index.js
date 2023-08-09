f56b124ffabacc1f598ea20999be9ce08eac1f95
Aman4587 <3pointaman@gmail.com>
2023-08-09 13:46:14 -0700

    DONEEEEEEEEEE

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ var showing = false;
 document.querySelector("img").style.display = "none";
 
 function show_image() {
-
-
+    document.querySelector("img").style.display = "block";
+    showing = true
+    document.querySelector("h1").style.display = "block";
 }
 
 function hide_image(){
-
+    document.querySelector("img").style.display = "none";
+    document.querySelector("h1").style.display = "none";
+    showing = false
 
 }
  
 function bp() {
-
-    
+    if (showing == true){
+        hide_image()
+    }
+    else{
+        show_image()
+    }
 }

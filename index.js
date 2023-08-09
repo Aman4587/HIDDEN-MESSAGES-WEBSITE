b66cca54efb05dd929604e99eed55e11fa257b8d
Aman4587 <3pointaman@gmail.com>
2023-08-09 14:05:31 -0700

    Added Colors to text

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var showing = false;
 document.querySelector("img").style.display = "none";
+colors = ["crimson","aliceblue","antiquewhite","aquamarine","azure","beige","bisque","blanchedalmond"]
+
 
 function show_image() {
     document.querySelector("img").style.display = "block";
     showing = true
     document.querySelector("h1").style.display = "block";
+    c = colors [Math.floor(Math.random()*colors.length)]
+    document.querySelector("h1").style.color = c
 }
 
 function hide_image(){

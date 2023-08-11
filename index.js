6920a7a7c8525b87fa60f9e0689329dd691e6045
Aman4587 <3pointaman@gmail.com>
2023-08-11 13:17:58 -0700

    FINISHED THE SPOOKY WEBSITE
    😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var showing = false;
 document.querySelector("img").style.display = "none";
 colors = ["crimson","aliceblue","antiquewhite","aquamarine","azure","beige","bisque","blanchedalmond"]
-
+messages = ["You are wearing a grey nike shirt I can see you","Are you in the dead???? Watch behind you 💀","IM WATCHING YOU 👀","What are you doing alone in the dark....","I KNOW WHEN TO DO SOMETHING, BUT WHAT IS IT??????????????????"]
 
 function show_image() {
     document.querySelector("img").style.display = "block";
@@ -9,6 +9,8 @@ function show_image() {
     document.querySelector("h1").style.display = "block";
     c = colors [Math.floor(Math.random()*colors.length)]
     document.querySelector("h1").style.color = c
+    rm = messages [Math.floor(Math.random()*messages.length)]
+    document.querySelector("h1").textContent = rm
 }
 
 function hide_image(){
